ac97b8cffe00c375975f3ac23522d34e6739cc3d
thomasdevl <91684310+thomasdevl@users.noreply.github.com>
2021-11-25 21:41:25 +0100

    Update opening.py

--- Projet 3/opening.py
+++ Projet 3/opening.py
@@ -1,7 +1,6 @@
 from sense_hat import SenseHat
 import time
 
-
 def opening():
     
     s = SenseHat()
@@ -10,8 +9,7 @@ def opening():
     white = (255,255,255)
     nothing = (0,0,0)
 
-
-    def twosquare():
+    def spiral1():
         W = white
         O = nothing
         logo = [
@@ -26,9 +24,25 @@ def opening():
         ]
         return logo
 
-    def threesquare():
-        O = nothing
+    def spiral1():
         W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, W, W, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+
+    def spiral2():
+        W = white
+        O = nothing
         logo = [
         O, O, O, O, O, O, O, O,
         O, O, O, O, O, O, O, O,
@@ -41,7 +55,82 @@ def opening():
         ]
         return logo
 
-    def foursquare():
+    def spiral3():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, W, W, W, O, O, O,
+        O, O, W, W, W, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo    
+      
+    def spiral4():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, W, W, O, O, O, O,
+        O, O, W, W, W, O, O, O,
+        O, O, W, W, W, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral5():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, W, W, W, W, O, O,
+        O, O, W, W, W, O, O, O,
+        O, O, W, W, W, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral6():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, W, W, W, W, O, O,
+        O, O, W, W, W, W, O, O,
+        O, O, W, W, W, W, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral7():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, W, W, W, W, O, O,
+        O, O, W, W, W, W, O, O,
+        O, O, W, W, W, W, O, O,
+        O, O, O, O, W, W, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral8():
         W = white
         O = nothing
         logo = [
@@ -55,8 +144,143 @@ def opening():
         O, O, O, O, O, O, O, O,
         ]
         return logo
+        
+    def spiral9():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, W, W, W, W, O, O,
+        O, O, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
 
-    def fivesquare():
+    def spiral10():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral11():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, W, W, O, O, O, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral12():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, W, W, W, W, O, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral13():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral14():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, O, O,
+        O, W, W, W, W, W, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral15():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral16():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, O, O, O, O, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral17():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, O, O, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral18():
         W = white
         O = nothing
         logo = [
@@ -71,9 +295,204 @@ def opening():
         ]
         return logo
 
-    def fullsquare():
-        O = nothing
+    def spiral19():
         W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral20():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        O, W, W, W, W, W, W, O,
+        O, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral21():
+        W = white
+        O = nothing
+        logo = [
+        O, O, O, O, O, O, O, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral22():
+        W = white
+        O = nothing
+        logo = [
+        W, W, O, O, O, O, O, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral23():
+        W = white
+        O = nothing
+        logo = [
+        W, W, W, W, O, O, O, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral24():
+        W = white
+        O = nothing
+        logo = [
+        W, W, W, W, W, W, O, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral25():
+        W = white
+        O = nothing
+        logo = [
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral26():
+        W = white
+        O = nothing
+        logo = [
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral27():
+        W = white
+        O = nothing
+        logo = [
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, O,
+        W, W, W, W, W, W, W, O,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral28():
+        W = white
+        O = nothing
+        logo = [
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        O, O, O, O, O, O, O, O,
+        ]
+        return logo
+
+    def spiral29():
+        W = white
+        O = nothing
+        logo = [
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        O, O, O, O, O, O, W, W,
+        ]
+        return logo
+
+    def spiral30():
+        W = white
+        O = nothing
+        logo = [
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        O, O, O, O, W, W, W, W,
+        ]
+        return logo
+
+    def spiral31():
+        W = white
+        O = nothing
+        logo = [
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        W, W, W, W, W, W, W, W,
+        O, O, W, W, W, W, W, W,
+        ]
+        return logo
+
+    def spiral32():
+        W = white
+        O = nothing
         logo = [
         W, W, W, W, W, W, W, W,
         W, W, W, W, W, W, W, W,
@@ -86,10 +505,12 @@ def opening():
         ]
         return logo
 
-    images = [twosquare, threesquare, foursquare, fivesquare, fullsquare]
 
-    while True: 
-        for count in range(len(images)):
-            s.set_pixels(images[count % len(images)]())
-            time.sleep(.25)
-        break
+    images = [spiral1, spiral2, spiral3, spiral4, spiral5, spiral6, spiral7, spiral8, spiral9, spiral10, spiral11, spiral12, spiral13, spiral14, spiral15, spiral16, spiral17, spiral18, spiral19, spiral20, spiral21, spiral22, spiral23, spiral24, spiral25, spiral26, spiral27, spiral28, spiral29, spiral30, spiral31, spiral32]
+    count = 0
+
+    while True:
+      for count in range(len(images)):
+        s.set_pixels(images[count % len(images)]())
+        time.sleep(.03)
+      break
